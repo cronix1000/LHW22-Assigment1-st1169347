699c6ca60612236f46dd6e722e4f795a275149b4
Idan Gomberg <crackenyogs10@gmail.com>
2022-02-16 22:27:01 -0500

    Finished fmxl skeleton and changed HelloController to main

---
+++ src/main/java/com/example/lhw22assigment1st1169347/StudentController.java
@@ -0,0 +1,25 @@
+package com.example.lhw22assigment1st1169347;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
+public class StudentController {
+
+    @FXML
+    private ListView<?> activityList;
+
+    @FXML
+    private Label firstNameLabel;
+
+    @FXML
+    private Label lastNameLabel;
+
+    @FXML
+    private ImageView studentImageView;
+
+    @FXML
+    private Label studentNumberLabel;
+
+}
